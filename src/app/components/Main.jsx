56044a9c7a5e89743391dc9fb2f39e439823ba24
Wisdom Anaba <50532579+wisdomanaba@users.noreply.github.com>
2020-08-11 02:10:24 +0100

    Update Main.jsx

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import { store } from '../store'
-import { ConnectedDashboard } from './Dashboard'
+// import { Provider } from 'react-redux'
+// import { store } from '../store'
+// import { ConnectedDashboard } from './Dashboard'
 
 export const Main = () => {
       return(
             <div>
                   <h1>From Javascript</h1>
-<!--                   <Provider store={ store }>
-                        <ConnectedDashboard />
-                  </Provider> -->
             </div>
       )
 }
